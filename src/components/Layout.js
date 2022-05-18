f76f790a0eb1aec5c0a5f561c03197663d41f2e0
mush-mush-mush <shroom.mish12@gmail.com>
2022-05-18 19:42:09 +0800

    fix metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -30,12 +30,47 @@ export default function Layout({ children, location }) {
     }
   }
 
-  console.log(mousePosition)
   return (
     <motion.div
       className="layout"
       onMouseMove={e => setMousePosition(getCursorCoordinates(e))}
     >
+      <Helmet>
+        <title>Made by Cello</title>
+        <meta
+          name="description"
+          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+
+        <meta property="og:url" content="https://madebycello.netlify.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Made by Cello" />
+        <meta
+          property="og:description"
+          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
+        />
+        <meta
+          property="og:image"
+          content="https://madebycello.netlify.app/og.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="madebycello.netlify.app" />
+        <meta
+          property="twitter:url"
+          content="https://madebycello.netlify.app/"
+        />
+        <meta name="twitter:title" content="Made by Cello" />
+        <meta
+          name="twitter:description"
+          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
+        />
+        <meta
+          name="twitter:image"
+          content="https://madebycello.netlify.app/og.png"
+        />
+      </Helmet>
       <motion.div
         className="cursor cursor--lg"
         initial={{ x: -20, y: -20 }}
