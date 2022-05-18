667233fbc528bb19bf87eeedf28d92fe57dcf024
mush-mush-mush <shroom.mish12@gmail.com>
2022-05-18 19:43:41 +0800

    fix metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import Helmet from "react-helmet"
 import { motion } from "framer-motion"
 import Footer from "./Footer"
 import Header from "./Header"
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,42 +9,6 @@ import Works from "../components/sections/Works"
 export default function Home({ location }) {
   return (
     <Layout location={location}>
-      <Helmet>
-        <title>Made by Cello</title>
-        <meta
-          name="description"
-          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
-        />
-        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
-
-        <meta property="og:url" content="https://madebycello.netlify.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Made by Cello" />
-        <meta
-          property="og:description"
-          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
-        />
-        <meta
-          property="og:image"
-          content="https://madebycello.netlify.app/og.png"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="madebycello.netlify.app" />
-        <meta
-          property="twitter:url"
-          content="https://madebycello.netlify.app/"
-        />
-        <meta name="twitter:title" content="Made by Cello" />
-        <meta
-          name="twitter:description"
-          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
-        />
-        <meta
-          name="twitter:image"
-          content="https://madebycello.netlify.app/og.png"
-        />
-      </Helmet>
       <Hero />
       <About />
       <Works />
